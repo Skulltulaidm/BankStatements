50e34fdf87fd8855f91988c1f312a3bd3bfbaca0
Skulltulaidm <a01612435@itesm.mx>
2023-09-11 13:31:17 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
-import openpyxl
-import streamlit as st
 import re
 import pdfplumber
 import pandas as pd
+import streamlit as st
+from io import BytesIO
+import base64
 
 def process_monex(file):
     all_text = ""
@@ -12,18 +13,27 @@ def process_monex(file):
 
     pattern = re.compile(r'(\d{2}/\w{3})\s+([\w\s]+?)(?:\s+(\d{8}))?\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(\d+\.\d{2}|\d{1,3}(?:,\d{3})*\.\d{2})\s+(-?\d+\.\d{2}|-?\d{1,3}(?:,\d{3})*\.\d{2})\s+(-?\d+\.\d{2}|-?\d{1,3}(?:,\d{3})*\.\d{2})')
 
-    matches = find_matches(all_text, pattern)
-
+    matches = [m.groups() for m in re.finditer(pattern, all_text)]
     column_names = ['Fecha', 'Descripción', 'Referencia', 'Abonos', 'Cargos', 'Movimiento Garantía', 'Saldo No Disponible', 'Saldo Disponible', 'Saldo Total']
     data = [dict(zip(column_names, match)) for match in matches]
 
     df = pd.DataFrame(data)
     return df
 
-def find_matches(text, pattern):
-    return [m.groups() for m in re.finditer(pattern, text)]
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
 
-# Streamlit code
+def get_table_download_link(df, filename='data.xlsx', text='Descargar archivo Excel'):
+    val = to_excel(df)
+    b64 = base64.b64encode(val).decode()
+    return f'<a href="data:application/octet-stream;base64,{b64}" download="{filename}">{text}</a>'
+
+# Streamlit UI
 st.title('Procesador de Estados de Cuenta')
 
 bank_option = st.selectbox('Selecciona tu banco:', ('MONEX', 'Otro banco'))
@@ -41,11 +51,6 @@ if st.button('Descargar'):
             dfs.append(df)
 
     if dfs:
-        # Aquí debes decidir cómo combinar los DataFrames en dfs si hay más de uno.
-        # Por ejemplo, puedes concatenarlos uno debajo del otro:
         final_df = pd.concat(dfs, ignore_index=True)
-
-        # Guardar y descargar el archivo Excel
-        excel_file = 'Estados_de_Cuenta.xlsx'
-        final_df.to_excel(excel_file, index=False)
-        st.write(f'Se han descargado {len(dfs)} estados de cuenta en formato Excel.')
+        st.write(f'Se han procesado {len(dfs)} estados de cuenta.')
+        st.markdown(get_table_download_link(final_df), unsafe_allow_html=True)
